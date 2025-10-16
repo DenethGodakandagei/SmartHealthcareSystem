a7a19f8126572a1320a769fe491b550b67d01071
Deneth  Godakandagei <contact.deneth@gmail.com>
2025-10-16 22:18:50 +0530

    route updated

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,6 +4,7 @@ import patientRoutes from "./patient.route";
 import HealthcareReportRoutes from "./HealthcareReport.routes";
 import doctorRoutes from "./doctor.routes";
 import appointmentRoutes from "./appointment.routes";
+import recordRoutes from "./record.routes.js";
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.use("/patients", patientRoutes);
 router.use("/reports", HealthcareReportRoutes);
 router.use("/doctors", doctorRoutes);
 router.use("/appointments", appointmentRoutes);
+router.use("/records", recordRoutes);
 
 export default router;
